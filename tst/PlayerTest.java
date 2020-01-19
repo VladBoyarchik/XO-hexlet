d270e3eb62d4fa90e37ec2a8e2db59d8ccbaea28
Vlad <vlad.boyarchik@gmail.com>
2020-01-19 15:16:07 +0300

    create PlayerTest

--- tst/PlayerTest.java
+++ tst/PlayerTest.java
@@ -1,3 +1,5 @@
+import io.hexlet.xo.model.Figure;
+import io.hexlet.xo.model.Player;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -6,9 +8,22 @@ public class PlayerTest {
 
     @Test
     public void testGetName() throws  Exception {
+        final String inputValue = "Vlad";
+        final String expectedValue = inputValue;
+
+        final Player player = new Player(inputValue,null);
+
+        final String actualValue = player.getName();
+
+        assertEquals(expectedValue, actualValue);
     }
 
     @Test
     public void testGetFigure() throws  Exception {
+        final Figure inputValue = Figure.X;
+        final Figure expectedValue = inputValue;
+        final Player player = new Player(null, inputValue);
+        final Figure actualValue = player.getFigure();
+        assertEquals(expectedValue, actualValue);
     }
 }
