c0ae35ab86ea9d3ee85bf17b944ee1617be264c6
Vlad <vlad.boyarchik@gmail.com>
2020-01-19 14:49:29 +0300

    create Player

---
+++ tst/PlayerTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PlayerTest {
+
+    @Test
+    public void testGetName() throws  Exception {
+    }
+
+    @Test
+    public void testGetFigure() throws  Exception {
+    }
+}
