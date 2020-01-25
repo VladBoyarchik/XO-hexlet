7268dedcb8853dcd89c080cd24ddbecbc4d5710a
Vlad <vlad.boyarchik@gmail.com>
2020-01-25 15:48:46 +0300

    MoveController created

---
+++ src/controllers/WinnerController.java
@@ -0,0 +1,10 @@
+package controllers;
+
+import io.hexlet.xo.model.Field;
+import io.hexlet.xo.model.Figure;
+
+public class WinnerController {
+
+   public Figure getWinner(final Field field) {
+}
+}
